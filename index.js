4c126d94eff0ed37180d42fedac921520fb343dd
mayurkandor <mayur.kharat@kandorsolutions.com>
2023-08-03 15:14:36 +0530

    new changes

--- index.js
+++ index.js
@@ -5,6 +5,44 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
+const pool = mysql.createPool({
+  host: "13.54.118.91",
+  user: "root",
+  password: "admin123",
+  database: "portal",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+pool.query(
+  `
+  CREATE TABLE IF NOT EXISTS employees (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL
+  )
+`,
+  (err, results) => {
+    if (err) {
+      console.error('Error creating "employees" table:', err);
+    } else {
+      console.log('Table "employees" created or already exists.');
+    }
+  }
+);
+
+app.get("/", (req, res) => {
+  pool.query("SELECT * FROM employees", (err, results) => {
+    if (err) {
+      console.error("Error executing MySQL query:", err);
+      res.status(500).send("Error fetching data from database");
+      return;
+    }
+    res.json(results);
+  });
+});
+
 app.get("/info", (req, res) => {
   res.send("hey this is /info api");
 });
